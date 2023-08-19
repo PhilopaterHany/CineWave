814d4aec96895733fe9d525bdcfa6125be4c3d0b
Philopater Hany <philopaterkiro@gmail.com>
2023-08-19 06:54:46 +0300

    Added header, footer and icons

--- CineWave_Frontend/src/app/app.module.ts
+++ CineWave_Frontend/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './Components/main-page/main-page.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- style-guide.md
+++ style-guide.md
@@ -11,26 +11,29 @@ The designs were created to the following widths:
 
 ### Primary
 
--
+-   Moderate Cyan: rgb(68, 191, 194)
 
 ### Secondary
 
--
--
+-   Keppel: rgb(55, 161, 164)
+
+### Neutral
+
+-   Gun Metal: rgb(69, 85, 90)
+-   Black Rock: rgb(46, 50, 62)
+-   Bombay: rgb(173, 178, 183)
+-   Oslo Grey: rgb(129, 135, 140)
+-   Athens Grey: rgb(238, 238, 243)
+-   Whitish Grey: rgb(247, 247, 247)
 
 ## Typography
 
 ### Font
 
--   Family: [](https://fonts.google.com/)
--   Weights: 400, 500
+-   Family: [Nanum Gothic](https://fonts.google.com/specimen/Nanum+Gothic)
+-   Weights: 400, 700, 900
 
 ---
 
--   Family: [](https://fonts.google.com/)
--   Weights: 400
-
----
-
--   Family: [](https://fonts.google.com/)
+-   Family: [Aboreto](https://fonts.google.com/specimen/Aboreto)
 -   Weights: 400
