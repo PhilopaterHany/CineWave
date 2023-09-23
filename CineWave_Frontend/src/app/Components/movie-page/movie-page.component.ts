29bd724504a91a219e43ec6b1a21f3726107ea71
Philopater Hany <philopaterkiro@gmail.com>
2023-09-23 13:52:45 +0300

    Added Dynamic movie-page

--- CineWave_Frontend/src/app/Components/movie-page/movie-page.component.ts
+++ CineWave_Frontend/src/app/Components/movie-page/movie-page.component.ts
@@ -1,17 +1,56 @@
-import { Component } from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {MovieDataService} from "../../Services/movie-data.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieDataService } from '../../Services/movie-data.service';
 
 @Component({
   selector: 'app-movie-page',
   templateUrl: './movie-page.component.html',
-  styleUrls: ['./movie-page.component.css']
+  styleUrls: ['./movie-page.component.css'],
 })
-export class MoviePageComponent {
+export class MoviePageComponent implements OnInit {
+  movieMetadata: any;
+  genreList: string[] = [];
 
-  movieMetadata: Object | undefined;
-  constructor(private route: ActivatedRoute, private router: Router, private movieDataService: MovieDataService) {
-    console.log(this.movieDataService.getMovieMetaData());
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private movieDataService: MovieDataService
+  ) {}
+
+  ngOnInit() {
+    this.movieMetadata = this.movieDataService.getMovieMetaData();
+
+    if (!this.movieMetadata) {
+      const sessionStorageData = sessionStorage.getItem('movie-data');
+      if (sessionStorageData) {
+        this.movieMetadata = JSON.parse(sessionStorageData);
+      }
+    }
+
+    if (!sessionStorage.getItem('movie-data')) {
+      sessionStorage.setItem('movie-data', JSON.stringify(this.movieMetadata));
+    }
+
+    if (this.movieMetadata && this.movieMetadata.Runtime) {
+      const runtimeMinutes = parseInt(this.movieMetadata.Runtime);
+      const hours = Math.floor(runtimeMinutes / 60);
+      const minutes = runtimeMinutes % 60;
+      this.movieMetadata.FormattedRuntime = `${hours}h ${minutes}min`;
+    }
+
+    if (this.movieMetadata && this.movieMetadata.imdbVotes) {
+      const votes = parseInt(this.movieMetadata.imdbVotes.replaceAll(",", ""));
+      if (votes >= 1000000) {
+        this.movieMetadata.FormattedVotes = `${(votes / 1000000).toFixed(1)}M`;
+      } else if (votes >= 1000) {
+        this.movieMetadata.FormattedVotes = `${(votes / 1000).toFixed(1)}K`;
+      } else {
+        this.movieMetadata.FormattedVotes = votes;
+      }
+    }
+
+    if (this.movieMetadata && this.movieMetadata.Genre) {
+      this.genreList = this.movieMetadata.Genre.split(', ');
+    }
   }
-
 }
