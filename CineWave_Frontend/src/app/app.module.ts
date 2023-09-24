28c21119514bfe65b917ad72c710754b7b87b772
Philopater Hany <philopaterkiro@gmail.com>
2023-09-25 00:15:11 +0300

    Favorites page v1.0 added
    Further enhancement will be done soon

--- CineWave_Frontend/src/app/app.module.ts
+++ CineWave_Frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +27,7 @@ import { WatchedMoviesPageComponent } from './Components/watched-movies-page/wat
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
